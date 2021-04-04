2badf34d1ef066d757e977a4712156c2d971b412
Salman Hasan <salmannhassan39@gmail.com>
2021-04-04 13:06:12 +0300

    Fix #1: Now images inside <a> tags work

--- preload-md.js
+++ preload-md.js
@@ -4,8 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
         node.addEventListener("click", event => {
             if (event.target) {
                 event.preventDefault();
-                let link = event.target.href;
-                require("electron").shell.openExternal(link);
+                if (node != event.target) {
+                    // Then it isn't the <a> tag it is it's childNode.
+                    let nodeList = event.path;
+                    for (let i = 0; i <= nodeList.length; i++) {
+                        // Search the path up the tree till you find the first <a> tag.
+                        if (nodeList[i].tagName === "A") {
+                            // use that <a> tag to get link.
+                            let link = nodeList[i].href;
+                            require("electron").shell.openExternal(link);
+                            return;
+                        }
+                    }
+                } else {
+                    let link = event.target.href;
+                    require("electron").shell.openExternal(link);
+                }
             }
         });
     });
